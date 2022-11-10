e7acbc82e98b7b782c252e5d07656b5540e5b7fa
Tyler Waugh <waughphoto@gmail.com>
2022-11-09 19:23:04 -0500

    Right Side Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
-import { Search } from '@material-ui/icons'
-import React from 'react'
-import styled from 'styled-components'
+import { Badge } from "@material-ui/core";
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
+import React from "react";
+import styled from "styled-components";
 
 const Container = styled.div`
     height: 60px;    
@@ -44,7 +45,14 @@ const Logo = styled.h1`
 `
 
 const Right = styled.div`
-     flex: 1;`
+     flex: 1;
+     align-items: center;
+  justify-content: flex-end;`
+
+const MenuItem = styled.div`
+font-size: 14px;
+cursor: pointer;
+margin-left: 25px;`
 
 
 
@@ -60,7 +68,15 @@ const Navbar = () => {
                 </SearchContainer>
             </Left>
             <Center><Logo>Butterscotch's Bakery</Logo></Center>
-            <Right>right</Right>
+            <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
+        </Right>
         </Wrapper>
     </Container>
   )
