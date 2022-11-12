795bcadaf98fa65350182934ce42886fde300f7a
Tyler Waugh <waughphoto@gmail.com>
2022-11-12 16:23:39 -0500

    Products

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -9,12 +9,13 @@ const Container = styled.div`
 `;
 
 const Categories = () => {
-  return <Container>
-    {categories.map(item => (
-        <CategoryItem item={item}/>
-    ))}
-    </Container>
-  
+  return ( 
+    <Container>
+      {categories.map((item) => (
+        <CategoryItem item={item} key={item.id} />
+      ))}
+     </Container>
+  );
 }
 
-export default Categories
+export default Categories; 
--- src/data.js
+++ src/data.js
@@ -39,3 +39,45 @@ export const sliderItems = [
       title: "LIGHT JACKETS",
     },
   ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+      id:2,
+      img:"https://images.unsplash.com/photo-1600369671236-e74521d4b6ad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80",
+    },
+    {
+      id:3,
+      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+      id:4,
+      img:"https://images.unsplash.com/photo-1618397360709-9dd900837411?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1042&q=80",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+      id:7,
+      img:"https://images.unsplash.com/photo-1535294435445-d7249524ef2e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+  ]
+
+// ID 2: Photo by <a href="https://unsplash.com/@jordanbigs?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+
+// ID: 4: Photo by <a href="https://unsplash.com/@jesperados?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jesper Brouwers</a> on <a href="https://unsplash.com/s/photos/Dog-Toy?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+
+// ID: 7 Photo by <a href="https://unsplash.com/@hypethat?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Chris Jarvis</a> on <a href="https://unsplash.com/s/photos/Dog-Toy?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
