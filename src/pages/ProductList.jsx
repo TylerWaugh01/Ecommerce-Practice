5c36c0fc334f021a3de49b8906a7581bbc6e3a04
Tyler Waugh <waughphoto@gmail.com>
2022-11-26 22:02:03 -0500

    Product List Responsive

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,47 +1,52 @@
 import styled from "styled-components";
 import Navbar from "../components/Navbar";
-import Annoucement from "../components/Annoucement";
+import Announcement from "../components/Announcement";
 import Products from "../components/Products";
 import Newsletter from "../components/Newsletter";
 import Footer from "../components/Footer";
+import { mobile } from "../responsive";
 
-
-const Container = styled.div``
+const Container = styled.div``;
 
 const Title = styled.h1`
-margin: 20px;`
+  margin: 20px;
+`;
 
 const FilterContainer = styled.div`
-display: flex;
-justify-content: space-between;
-`
+  display: flex;
+  justify-content: space-between;
+`;
+
 const Filter = styled.div`
-margin: 20px;`
+  margin: 20px;
+  ${mobile({ width: "0px 20px", display: "flex", flexDirection: "column" })}
+`;
 
 const FilterText = styled.span`
   font-size: 20px;
   font-weight: 600;
   margin-right: 20px;
+  ${mobile({ marginRight: "0px" })}
 `;
 
 const Select = styled.select`
   padding: 10px;
   margin-right: 20px;
+  ${mobile({ margin: "10px 0px" })}
 `;
-
 const Option = styled.option``;
 
 const ProductList = () => {
   return (
-   <Container>
-    <Navbar />
-    <Annoucement />
-    <Title>Dresses</Title>
-    <FilterContainer>
+    <Container>
+      <Navbar />
+      <Announcement />
+      <Title>Dresses</Title>
+      <FilterContainer>
         <Filter>
-            <FilterText>Filter Products:</FilterText>
-        <Select>
-        <Option disabled selected>
+          <FilterText>Filter Products:</FilterText>
+          <Select>
+            <Option disabled selected>
               Color
             </Option>
             <Option>White</Option>
@@ -49,9 +54,9 @@ const ProductList = () => {
             <Option>Red</Option>
             <Option>Blue</Option>
             <Option>Yellow</Option>
-            <Option>Green</Option>    
-        </Select>
-        <Select>
+            <Option>Green</Option>
+          </Select>
+          <Select>
             <Option disabled selected>
               Size
             </Option>
@@ -63,18 +68,19 @@ const ProductList = () => {
           </Select>
         </Filter>
         <Filter>
-            <FilterText>Sort Products:</FilterText>
-            <Select>
+          <FilterText>Sort Products:</FilterText>
+          <Select>
             <Option selected>Newest</Option>
             <Option>Price (asc)</Option>
             <Option>Price (desc)</Option>
           </Select>
         </Filter>
-    </FilterContainer>
-    <Products /> 
-    <Newsletter />
-   </Container>
-  )
-}
+      </FilterContainer>
+      <Products />
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
+};
 
-export default ProductList
+export default ProductList;
