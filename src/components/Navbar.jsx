6ec1d06245c5b94c34627eb83fc5d3c9284ae1df
Tyler Waugh <waughphoto@gmail.com>
2023-04-15 10:51:06 -0400

    Navbar Design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,87 @@
-import React from 'react'
+import { Badge } from "@material-ui/core";
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 60px;
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex; // Wrapper is parent of Left/Center/Right, flexbox to make horizontal
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center; // Everything in SC center to match language/left
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+ border: none;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const MenuItem = styled.div`
+    font-size: 14;
+    cursor: pointer;
+    margin-left: 25px;
+`;
 
 export const Navbar = () => {
   return (
-    <div>
-        navbar
-    </div>
-  )
-}
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input />
+            <Search style={{ color: "gray", fontSize: 16 }}/>
+          </SearchContainer>
+        </Left>
+        <Center><Logo>Butter.</Logo></Center>
+        <Right>
+            <MenuItem>REGISTER</MenuItem>
+            <MenuItem>SIGN IN</MenuItem>
+            <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>   
+            </MenuItem>
+
+
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+};
