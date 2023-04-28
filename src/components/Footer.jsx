f0cf627303bfe0aa465ec9524e6a076f2150a00a
Tyler Waugh <waughphoto@gmail.com>
2023-04-27 22:39:55 -0400

    Left

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,67 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+display: flex;
+`;
+
+const Left = styled.div`  
+`
+
+const Center = styled.div`  
+flex: 1;
+`
+
+const Right = styled.div`  
+flex: 1;
+`
+
+/** const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+
+**/
 
 
 export const Footer = () => {
   return (
     <Container>
-        Footer
-
+        <Left>
+        <Logo>LAMA.</Logo>
+        <Desc>
+          There are many variations of passages of Lorem Ipsum available, but
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised words which don’t look even slightly believable.
+        </Desc>
+        <SocialContainer>
+          <SocialIcon color="3B5999">
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon color="E4405F">
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon color="55ACEE">
+            <Twitter />
+          </SocialIcon>
+          <SocialIcon color="E60023">
+            <Pinterest />
+          </SocialIcon>
+        </SocialContainer>
+        </Left>
+        <Center></Center>
+        <Right></Right>
     </Container>
   )
 }
