0bbc8e12f665c2ac83d46fe4eb349447f9db2fa4
Tyler Waugh <waughphoto@gmail.com>
2023-04-30 10:07:54 -0400

    Center Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,54 +1,56 @@
 import {
-    Facebook,
-    Instagram,
-    MailOutline,
-    Phone,
-    Pinterest,
-    Room,
-    Twitter,
-  } from "@material-ui/icons";
-import styled from 'styled-components';
+  Facebook,
+  Instagram,
+  MailOutline,
+  Phone,
+  Pinterest,
+  Room,
+  Twitter,
+} from "@material-ui/icons";
+import styled from "styled-components";
 
 const Container = styled.div`
-display: flex;
+  display: flex;
 `;
 
-const Left = styled.div`  
-flex: 1;
-display: flex;
-flex-direction: column;
-padding: 20px;
-`
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
 
-const Logo = styled.h1``
+const Logo = styled.h1``;
 
-const Desc = styled.p``
+const Desc = styled.p``;
 
 const SocialContainer = styled.div`
-display: flex;
-`
+  display: flex;
+`;
 
 const SocialIcon = styled.div`
-width: 40px;
-height: 40px;
-border-radius: 50%;
-color: white;
-background-color: #${(props) => props.color};
-display: flex;
-align-items: center;
-justify-content: center;
-margin-right: 20px;
-`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`;
 
-const Center = styled.div`  
-flex: 1;
-`
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
 
-const Right = styled.div`  
-flex: 1;
-`
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
 
-/** const Title = styled.h3`
+const Title = styled.h3`
   margin-bottom: 30px;
 `;
 
@@ -57,21 +59,18 @@ const List = styled.ul`
   padding: 0;
   list-style: none;
   display: flex;
-  flex-wrap: wrap;
+  flex-wrap: wrap; 
 `;
 
 const ListItem = styled.li`
   width: 50%;
-  margin-bottom: 10px;
+  margin-bottom: 10px; 
 `;
-
-**/
-
 
 const Footer = () => {
   return (
     <Container>
-        <Left>
+      <Left>
         <Logo>LAMA.</Logo>
         <Desc>
           There are many variations of passages of Lorem Ipsum available, but
@@ -92,11 +91,25 @@ const Footer = () => {
             <Pinterest />
           </SocialIcon>
         </SocialContainer>
-        </Left>
-        <Center></Center>
-        <Right></Right>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Woman Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right></Right>
     </Container>
-  )
-}
+  );
+};
 
 export default Footer;
