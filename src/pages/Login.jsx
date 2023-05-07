fbea7a64998d48173a9496616ee56e12f7c204c4
Tyler Waugh <waughphoto@gmail.com>
2023-05-07 18:06:43 -0400

    Register Page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+`;
+
+const Wrapper = styled.div`
+`;
+
+const Title = styled.h1`
+`;
+
+const Form = styled.form`
+`;
+
+const Input = styled.input`
+`;
+
+const Button = styled.button`
+`;
+
+const Link = styled.a`
+`;
+
+const Login = () => {
+  return (
+    <Container>
+    </Container>
+  );
+};
+
+export default Login;
