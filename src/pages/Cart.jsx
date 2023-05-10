a673b386dfcb4932de96948f138fbe85174d1dae
Tyler Waugh <waughphoto@gmail.com>
2023-05-10 16:31:21 -0400

    Cart Creation

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+
+const Cart = () => {
+  return (
+    <Container>
+ 
+    </Container>
+  );
+};
+
+export default Cart;
