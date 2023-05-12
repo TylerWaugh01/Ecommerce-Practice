ec53f29261226b09ae47788627919bc27d137577
Tyler Waugh <waughphoto@gmail.com>
2023-05-11 23:50:33 -0400

    Navbar + Announcement

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import Announcement from "../components/Announcement";
+import Navbar from "../components/Navbar";
 
 const Container = styled.div``;
 
@@ -6,7 +8,8 @@ const Container = styled.div``;
 const Cart = () => {
   return (
     <Container>
- 
+    <Navbar />
+      <Announcement />
     </Container>
   );
 };
