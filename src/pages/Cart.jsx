bc52105af21383c5c8903d52871ff50d2960b77f
Tyler Waugh <waughphoto@gmail.com>
2023-05-12 23:52:36 -0400

    Top Cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,12 +4,42 @@ import Navbar from "../components/Navbar";
 
 const Container = styled.div``;
 
+const Wrapper = styled.div`
+`;
+
+const Title = styled.h1`
+`;
+
+const Top = styled.div`
+`;
+
+const TopButton = styled.button`
+`;
+
+const TopTexts = styled.div`
+`;
+
+const TopText = styled.span`
+`;
+
 
 const Cart = () => {
   return (
     <Container>
     <Navbar />
       <Announcement />
+    <Wrapper>
+        <Title>YOUR BAG</Title>
+        <Top>
+          <TopButton>CONTINUE SHOPPING</TopButton>
+          <TopTexts>
+            <TopText>Shopping Bag(2)</TopText>
+            <TopText>Your Wishlist (0)</TopText>
+          </TopTexts>
+          <TopButton type="filled">CHECKOUT NOW</TopButton>
+        </Top>
+    </Wrapper>
+      
     </Container>
   );
 };
