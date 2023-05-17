9b54dd2e5022c31c693b322e13f890812dfae5af
Tyler Waugh <waughphoto@gmail.com>
2023-05-16 20:36:18 -0400

    Product Name

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -81,7 +81,9 @@ const Cart = () => {
           <PriceDetail>
             <Image src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1614188818-TD1MTHU_SHOE_ANGLE_GLOBAL_MENS_TREE_DASHERS_THUNDER_b01b1013-cd8d-48e7-bed9-52db26515dc4.png?crop=1xw:1.00xh;center,top&resize=480%3A%2A"/>
           <Details>
-            <ProductName>Jessie Thunder Shoes</ProductName>
+          <ProductName>
+                    <b>Product:</b> JESSIE THUNDER SHOES
+                  </ProductName>
           </Details>
           </PriceDetail>
         </Product>
