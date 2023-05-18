ec7f4ce77098457af1ed8bad0dec25f83f1aa58b
Tyler Waugh <waughphoto@gmail.com>
2023-05-17 22:52:37 -0400

    Product Size

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -84,6 +84,13 @@ const Cart = () => {
           <ProductName>
                     <b>Product:</b> JESSIE THUNDER SHOES
                   </ProductName>
+                  <ProductId>
+                    <b>ID:</b> 93813718293
+                  </ProductId>
+                  <ProductColor color="black" />
+                  <ProductSize>
+                    <b>Size:</b> 37.5
+                  </ProductSize>
           </Details>
           </PriceDetail>
         </Product>
