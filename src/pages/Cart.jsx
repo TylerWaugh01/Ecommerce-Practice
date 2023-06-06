f185ae7ddea5ed0fcd2da6be1960ccf54dfc4e09
Tyler Waugh <waughphoto@gmail.com>
2023-06-05 23:32:48 -0400

    Product Details

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,23 +1,22 @@
 import styled from "styled-components";
-import Navbar from "../components/Navbar"
+import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Annoucement from "../components/Annoucement";
 
-const Container = styled.div`
-`
+const Container = styled.div``;
 const Wrapper = styled.div`
   padding: 20px;
-`
+`;
 const Title = styled.h1`
   font-weight: 300;
   text-align: center;
-`
+`;
 const Top = styled.div`
-display: flex;
-align-items: center;
-justify-content: space-between;
-padding: 20px;
-`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+`;
 const TopButton = styled.button`
   padding: 10px;
   font-weight: 600;
@@ -26,30 +25,28 @@ const TopButton = styled.button`
   background-color: ${(props) =>
     props.type === "filled" ? "black" : "transparent"};
   color: ${(props) => props.type === "filled" && "white"};
-`
+`;
 
-const TopTexts = styled.div`
-`
+const TopTexts = styled.div``;
 
 const TopText = styled.span`
-text-decoration: underline;
-cursor: pointer;
-margin: 0px 10px;
-
-`
+  text-decoration: underline;
+  cursor: pointer;
+  margin: 0px 10px;
+`;
 
 const Bottom = styled.div`
-display: flex;
-justify-content: space-between;
-`
+  display: flex;
+  justify-content: space-between;
+`;
 
 const Info = styled.div`
-flex: 3;
-`
+  flex: 3;
+`;
 
 const Summary = styled.div`
-flex: 1;
-`
+  flex: 1;
+`;
 
 const Cart = () => {
   return (
@@ -57,32 +54,42 @@ const Cart = () => {
       <Navbar />
       <Annoucement />
       <Wrapper>
-       <Title>Your Bag</Title>
-       <Top>
-
-        <TopButton>Continue Shopping</TopButton>
-        <TopTexts>
+        <Title>Your Bag</Title>
+        <Top>
+          <TopButton>Continue Shopping</TopButton>
+          <TopTexts>
             <TopText>Shopping Bag(2)</TopText>
             <TopText>Your Wishlist (0)</TopText>
           </TopTexts>
           <TopButton type="filled">CHECKOUT NOW</TopButton>
-       </Top>
-       <Bottom>
-        <Info>
-<Product>
-  <ProductDetail>
-    <Image />
-  </ProductDetail>
-  <PriceDetail></PriceDetail>
-</Product>
-
-        </Info>
-        <Summary>summary</Summary>
-       </Bottom>
-        </Wrapper>
+        </Top>
+        <Bottom>
+          <Info>
+            <Product>
+              <ProductDetail>
+                <Image src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1614188818-TD1MTHU_SHOE_ANGLE_GLOBAL_MENS_TREE_DASHERS_THUNDER_b01b1013-cd8d-48e7-bed9-52db26515dc4.png?crop=1xw:1.00xh;center,top&resize=480%3A%2A" />
+                <Details>
+                  <ProductName>
+                    <b>Product:</b> JESSIE THUNDER SHOES
+                  </ProductName>
+                  <ProductId>
+                    <b>ID:</b> 93813718293
+                  </ProductId>
+                  <ProductColor/>
+                  <ProductSize>
+                    <b>Size:</b> 37.5
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetail></PriceDetail>
+            </Product>
+          </Info>
+          <Summary>summary</Summary>
+        </Bottom>
+      </Wrapper>
       <Footer />
     </Container>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
