5a8285e12dabec30747bf9e563a36ab488a3aa18
Tyler Waugh <waughphoto@gmail.com>
2023-06-27 23:31:15 -0400

    Home Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Cart from "./pages/Cart";
-// import Home from "./pages/Home";
-// import Login from "./pages/Login";
-// import Product from "./pages/Product";
-// import ProductList  from "./pages/ProductList";
-// import Register from "./pages/Register";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import ProductList  from "./pages/ProductList";
+import Register from "./pages/Register";
 
 const App = () => {
-  return <Cart/>
+  return 
+  <Home/>
+
 };
 
 export default App;
